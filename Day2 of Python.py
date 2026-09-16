ec308af20ee2920b5edfece4a191ad0d89bf5efb
Gautham krishna Ganta <143061668+gauthamkrishnaganta@users.noreply.github.com>
2026-09-16 10:14:01 +0530

    Add files via upload
    Python tokens (keywords, identifiers, literals)

---
+++ Day2 of Python.py
@@ -0,0 +1,105 @@
+'''
+Tokens
+--------
+->Tokens are the smallest individual parts or units in the code or program.
+
+-keywords
+Keywords are the resierved words which are already ....
+if
+for
+else
+return
+while
+
+-indentifiers
+variable
+function names
+class name
+
+-operators
++ - =
+
+-literal
+int, str, tuple, list
+
+operators:
+---------------
+1.arthamatic
+    + - / % * **
+num = 8
+num2 = 2
+print(num + num2)
+print(num - num2)
+print(num * num2)
+print(num / num2)
+print(num % num2)
+print(num ** num2)
+-------------------
+2.assignment
+    =, +=, -=, *=, %=, /=
+
+num = 8
+num2 = 2
+print(num += num2)
+print(num -= num2)
+print(num *= num2)
+print(num /= num2)
+print(num %= num2)
+print(num **= num2
+
+------------------------
+3.comparision
+    ==, !=, <=, >=, <, >
+    
+num = 8
+num2 = 7
+print(num == num2)
+print(num != num2)
+print(num <= num2)
+print(num >= num2)
+print(num < num2)
+--------------------------
+4.logical
+    and or not
+
+num = 78
+num2 = 89
+print(num < num2 and num2 < 100)
+print(num > num2 or num2 < 100)
+print(not(num < num2 and num2 < 100))
+
+-----------------------------------------
+5.identity
+    is
+    is not
+
+so = 1
+how = 1
+print(id(so))
+print(id(how))
+print(so is how)
+print(so is not how)
+---------------------------
+6.membership
+    in
+    not in
+    
+so = [ 1,2,3,4,5 ]
+print(7 in so)
+print(7 not in so)
+-------------------------
+7.bitwise
+    &, |, ^, <<, >>
+
+print(5 & 3)
+print(5 | 3)
+print(5 ^ 3)
+print(5 << 3)
+print(5 >> 3)
+'''
+
+
+
+
+
+
